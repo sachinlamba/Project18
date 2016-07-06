471a020cffcd0a975c1a674cf2cefb77950cc967
sachin lamba <sachin.lamba@moldedbits.com>
2016-07-06 11:58:53 +0530

    iAD banner is added

--- Project18/ViewController.swift
+++ Project18/ViewController.swift
@@ -5,14 +5,27 @@
 //  Created by Sachin Lamba on 05/07/16.
 //  Copyright © 2016 LambaG. All rights reserved.
 //
-
+import iAd
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ADBannerViewDelegate {
 
+    var bannerView: ADBannerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        bannerView = ADBannerView(adType: .Banner)
+        bannerView.translatesAutoresizingMaskIntoConstraints = false
+        bannerView.delegate = self
+        bannerView.hidden = true
+        view.addSubview(bannerView)
+        
+        let viewsDictionary = ["bannerView": bannerView]
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[bannerView]|", options: [], metrics: nil, views: viewsDictionary))
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[bannerView]|", options: [], metrics: nil, views: viewsDictionary))
+        
     }
 
     override func didReceiveMemoryWarning() {
