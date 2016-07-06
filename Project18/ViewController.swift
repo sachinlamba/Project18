6277ef034cdd1a806598b78d9951d2112bba697b
sachin lamba <sachin.lamba@moldedbits.com>
2016-07-06 13:53:04 +0530

    Submission of Project 18 without working iAD

--- Project18/ViewController.swift
+++ Project18/ViewController.swift
@@ -5,27 +5,14 @@
 //  Created by Sachin Lamba on 05/07/16.
 //  Copyright © 2016 LambaG. All rights reserved.
 //
-import iAd
+
 import UIKit
 
-class ViewController: UIViewController, ADBannerViewDelegate {
+class ViewController: UIViewController {
 
-    var bannerView: ADBannerView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        bannerView = ADBannerView(adType: .Banner)
-        bannerView.translatesAutoresizingMaskIntoConstraints = false
-        bannerView.delegate = self
-        bannerView.hidden = true
-        view.addSubview(bannerView)
-        
-        let viewsDictionary = ["bannerView": bannerView]
-        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[bannerView]|", options: [], metrics: nil, views: viewsDictionary))
-        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[bannerView]|", options: [], metrics: nil, views: viewsDictionary))
-        
     }
 
     override func didReceiveMemoryWarning() {
